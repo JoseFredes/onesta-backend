3475659b4b0163717f69b562988c2d8008a49023
josefredes <jose.fredes@mayor.cl>
2023-08-31 00:38:17 -0400

    feat(clients): update clients imports and add client dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { FruitsModule } from './fruits/fruits.module';
+import databaseConfig from './config/database.config';
+import { ClientsModule } from './clients/clients.module';
+import { FarmersModule } from './farming/farmers.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    FarmersModule,
+    ClientsModule,
+    FruitsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
