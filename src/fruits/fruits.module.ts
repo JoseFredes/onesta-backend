b1c84b24f5e5cb04d6b09551ff4213a2f3f88e4d
josefredes <jose.fredes@mayor.cl>
2023-08-31 01:03:45 -0400

    feat(fruits): add better folder structure

--- src/fruits/fruits.module.ts
+++ src/fruits/fruits.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
-import { FruitsService } from './fruits.service';
-import { FruitsController } from './fruits.controller';
+import { FruitsService } from './services/fruits.service';
+import { FruitsController } from './controllers/fruits.controller';
 import { Fruit } from './entities/fruit.entity';
 import { Harvest } from './entities/harvest.entity';
 import { Variety } from './entities/variety.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { VarietyService } from './services/variety.service';
+import { HarvestService } from './services/harvest.service';
+import { HarvestController } from './controllers/harvest.controller';
+import { VarietyController } from './controllers/variety.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Fruit, Harvest, Variety])],
-  providers: [FruitsService],
-  controllers: [FruitsController],
+  providers: [FruitsService, HarvestService, VarietyService],
+  controllers: [FruitsController, HarvestController, VarietyController],
 })
 export class FruitsModule {}
