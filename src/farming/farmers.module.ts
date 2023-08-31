fd684d745fb09d44ecc776a9edec97674a42c73d
josefredes <jose.fredes@mayor.cl>
2023-08-31 00:50:46 -0400

    feat(farming): add  better folder structure

--- src/farming/farmers.module.ts
+++ src/farming/farmers.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Farmer } from './entities/farmer.entity';
 import { Farm } from './entities/Farm.entity';
-import { FarmersService } from './farmers.service';
-import { FarmersController } from './farmers.controller';
+import { FarmersService } from './services/farmers.service';
+import { FarmersController } from './controllers/farmers.controller';
 import { FarmService } from './farm/farm.service';
 import { FarmsController } from './farms/farms.controller';
-import { FarmsService } from './farms.service';
+import { FarmsService } from './services/farms.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Farmer, Farm])],
--- src/farming/services/farms.service.spec.ts
+++ src/farming/services/farms.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { FarmsService } from '../farms.service';
+import { FarmsService } from './farms.service';
 
 describe('FarmsService', () => {
   let service: FarmsService;
