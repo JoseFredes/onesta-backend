a3f853e7523d75614768d4137e1d3a2be77b6210
josefredes <jose.fredes@mayor.cl>
2023-09-05 20:20:59 -0300

    feat(farmers): handle errors in controllers

--- src/farming/controllers/farmers.controller.ts
+++ src/farming/controllers/farmers.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  BadRequestException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { FarmersService } from '../services/farmers.service';
 import { Farmer } from '../entities/farmer.entity';
+import { EmailDto } from 'src/common/dto/common.dto';
 
 @Controller('farmers')
 export class FarmersController {
@@ -12,8 +22,21 @@ export class FarmersController {
   }
 
   @Get(':email')
-  findOne(@Param('email') email: string): Promise<Farmer> {
-    return this.farmerService.findOne(email);
+  async findOne(@Param() params: EmailDto): Promise<Farmer> {
+    const email = params.email;
+    if (!email) throw new BadRequestException('Invalid email format');
+
+    try {
+      const farmer = await this.farmerService.findOne(email);
+      if (!farmer)
+        throw new NotFoundException(`Farmer with ${email} not found`);
+
+      return farmer;
+    } catch (error) {
+      if (error instanceof NotFoundException) throw error;
+
+      throw new InternalServerErrorException('Error to get farmer');
+    }
   }
 
   @Post()
