067040d429641a1de268e206fbcaf50ef5245e39
josefredes <jose.fredes@mayor.cl>
2023-09-05 20:38:12 -0300

    feat(farm): add validation and Dto

--- src/farming/controllers/farms.controller.ts
+++ src/farming/controllers/farms.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  NotFoundException,
+  InternalServerErrorException,
+  BadRequestException,
+  ValidationPipe,
+} from '@nestjs/common';
 import { FarmsService } from '../services/farms.service';
 import { CreateFarmDto } from '../dto/farm.dto';
 import { Farm } from '../entities/farm.entity';
+import { NameDto } from 'src/common/dto/common.dto';
 
 @Controller('farms')
 export class FarmsController {
@@ -13,12 +24,33 @@ export class FarmsController {
   }
 
   @Get(':name')
-  findOne(@Param('name') name: string): Promise<Farm> {
-    return this.farmService.findOne(name);
+  async findOne(@Param() params: NameDto): Promise<Farm> {
+    const name = params.name;
+    if (!name) throw new BadRequestException('Invalid farm name format');
+
+    try {
+      const farm = await this.farmService.findOne(name);
+      if (!farm) {
+        throw new NotFoundException(`Farm with name ${name} not found`);
+      }
+      return farm;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error to get farm');
+    }
   }
 
   @Post()
-  create(@Body() createFarmDto: CreateFarmDto): Promise<Farm> {
-    return this.farmService.createFarm(createFarmDto);
+  create(@Body(ValidationPipe) farm: CreateFarmDto): Promise<Farm> {
+    try {
+      return this.farmService.createFarm(farm);
+    } catch (error) {
+      if (error.message.includes('Validation failed')) {
+        throw new BadRequestException(error.message);
+      }
+      throw new InternalServerErrorException('Error to create farm');
+    }
   }
 }
--- src/farming/dto/farm.dto.ts
+++ src/farming/dto/farm.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, MinLength, IsNotEmpty } from 'class-validator';
+import { IsString, MinLength, IsNotEmpty, IsUUID } from 'class-validator';
 
 export class CreateFarmDto {
   @IsString()
@@ -10,4 +10,9 @@ export class CreateFarmDto {
   @IsNotEmpty()
   @MinLength(3)
   location: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  farmerId: string;
 }
