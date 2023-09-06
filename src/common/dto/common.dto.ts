da0b1804f6406285534e27ae09f038c169a5beeb
josefredes <jose.fredes@mayor.cl>
2023-09-05 21:14:58 -0300

    feat(common): add common dto

--- src/common/dto/common.dto.ts
+++ src/common/dto/common.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsUUID } from 'class-validator';
 export class EmailDto {
   @IsEmail()
   email: string;
@@ -7,4 +7,10 @@ export class EmailDto {
 export class NameDto {
   @IsNotEmpty()
   name: string;
+}
+
+export class idDto {
+  @IsNotEmpty()
+  @IsUUID()
+  id: string;
 }
